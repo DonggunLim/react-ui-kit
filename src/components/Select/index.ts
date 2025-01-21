97c3b22ba3389cde5bbdbe95443a8aa6d364efc5
DonggunLim <Ldonggun6766@gmail.com>
2025-01-21 16:03:28 +0900

    feat: Select component 추가

---
+++ src/components/Select/index.ts
@@ -0,0 +1,13 @@
+import Root from "./components/Root";
+import Content from "./components/Content";
+import Item from "./components/Item";
+import Trigger from "./components/Trigger";
+
+const Select = {
+  Root,
+  Trigger,
+  Content,
+  Item,
+};
+
+export default Select;
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,16 +4,8 @@ export { default as Calendar } from "./Calendar";
 export { default as Breadcrumb } from "./Breadcrumb";
 export { default as Pagination } from "./Pagination";
 export { default as Accordion } from "./Accordion";
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-export { default as Popover } from "./Popover";
-export { default as Progress } from "./Popover";
-=======
-=======
->>>>>>> Stashed changes
 export { default as Progress } from "./Progress";
 export { default as Popover } from "./Popover";
 export { default as Select } from "./Select";
 export { default as Modal } from "./Modal";
 export { default as DatePicker } from "./DatePicker";
->>>>>>> Stashed changes

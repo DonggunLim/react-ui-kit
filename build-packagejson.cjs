58c09aab464fc1de7e751c9395452a00c417e608
DonggunLim <Ldonggun6766@gmail.com>
2025-11-26 11:44:37 +0900

    fix: update react depecdencies version

--- build-packagejson.cjs
+++ build-packagejson.cjs
@@ -1,22 +1,19 @@
 // build-packagejson.cjs
 const fs = require("fs");
 const path = require("path");
-const packageJson = require("./package.json");
 const buildDir = path.resolve(__dirname, "dist");
 const packageName = "@imdonggun/react-ui-kit";
 
 const getPackageJsonData = () => {
-  const { react: reactVersion, "react-dom": reactDomVersion } =
-    packageJson.dependencies;
   return {
-    version: "0.0.14",
+    version: "0.0.15",
     name: packageName,
     main: "./index.cjs",
     module: "./index.js",
     types: "./index.d.ts",
     peerDependencies: {
-      react: reactVersion,
-      "react-dom": reactDomVersion,
+      react: "^18.0.0 || ^19.0.0",
+      "react-dom": "^18.0.0 || ^19.0.0",
     },
     repository: {
       type: "git",
